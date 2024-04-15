f2b30321e7c289f87a2b2f985d0fe0e189e41bd0
Clay M - Home <140664778+Plattko@users.noreply.github.com>
2024-04-15 15:43:29 +1000

    Set Up Player & Added Player Movement
    - Set up Player in Game scene with Rigidbody2D, CircleCollider2D, PlayerInput, and Animator components, PlayerController script, and PlayerSprite child object with a SpriteRenderer.
    - Created PlayerController script that moves the player up, down, left, and right based on their move input.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Plattko
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private Rigidbody2D rb;
+        private Animator animator;
+
+        [Header("Movement Settings")]
+        private Vector2 moveInput;
+        [SerializeField]private float runSpeed = 8f;
+        [SerializeField]private float walkSpeed = 4f;
+        [SerializeField]private float idleSlow = 0.9f;
+
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            animator = GetComponent<Animator>();
+        }
+
+        void Update()
+        {
+            //Debug.Log(rb.velocity.normalized);
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+
+            //UpdateAnimationParameters();
+        }
+
+        // ---------------------------------
+        // MOVEMENT METHODS
+        // ---------------------------------
+        private void Move()
+        {
+            if (moveInput != Vector2.zero)
+            {
+                rb.velocity = moveInput * runSpeed;
+            }
+            else
+            {
+                rb.velocity = Vector2.Lerp(rb.velocity, Vector2.zero, idleSlow);
+            }
+        }
+
+        // ---------------------------------
+        // SPRITE AND ANIMATIONS
+        // ---------------------------------
+        private void UpdateAnimationParameters()
+        {
+            if (moveInput != Vector2.zero)
+            {
+                animator.SetFloat("Horizontal", moveInput.x);
+                animator.SetFloat("Vertical", moveInput.y);
+                animator.SetFloat("Speed", moveInput.sqrMagnitude);
+            }
+            else
+            {
+                animator.SetFloat("Speed", 0f);
+            }
+        }
+
+        // ---------------------------------
+        // INPUT CHECKS
+        // ---------------------------------
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            moveInput = context.ReadValue<Vector2>();
+        }
+
+        public void OnUse(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                //Functionality
+            }
+        }
+
+        public void OnInteract(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                //Functionality
+            }
+        }
+    }
+}
