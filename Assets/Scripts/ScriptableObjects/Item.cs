c83676941474c1c61026df5a05b4b94b397132df
Clay M - Home <140664778+Plattko@users.noreply.github.com>
2024-04-16 22:37:26 +1000

    Created Item Scriptable Object
    - Imported hoe sprite.
    - Created Item scriptable object script that contains the item type, action type, whether the item is stackable, its sprite, and its tilebase (if applicable).
    - Created Hoe item using Item scriptable object.

---
+++ Assets/Scripts/ScriptableObjects/Item.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Plattko
+{
+    [CreateAssetMenu(fileName = "NewItem", menuName = "Scriptable Object/Create Item", order = 0)]
+    public class Item : ScriptableObject
+    {
+        [Header ("Gameplay Only")]
+        public TileBase tile;
+        public ItemType itemType;
+        public ActionType actionType;
+
+        [Header("UI Only")]
+        public bool isItemStackable = true;
+
+        [Header ("UI and Gameplay")]
+        public Sprite image;
+
+        public enum ItemType
+        {
+            Undefined,
+            Tool,
+            PlaceableObject
+        }
+
+        public enum ActionType
+        {
+            Undefined,
+            Scythe,
+            Hoe,
+            WateringCan,
+            Pickaxe,
+            Axe,
+            Sword
+        }
+    }
+}
