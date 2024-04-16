d5e98dd07a800c280ede1935300bc0b559195776
Clay M - Home <140664778+Plattko@users.noreply.github.com>
2024-04-16 21:22:40 +1000

    Added Switching Action Maps
    - Updated GameControls input action asset to generate a GameControls C# Class.
    - Disabled PauseMenu object in Game scene.
    - Linked the Pause and Resume inputs to OnPause and OnResume in the MenuManager script in PlayerInput.
    - Updated PauseMenu script to switch between the Player and PauseMenu action map through PlayerInput.

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -1,28 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Plattko
 {
     public class MenuManager : MonoBehaviour
     {
+        [SerializeField] private PlayerInput playerInput;
         [SerializeField] private GameObject pauseMenuUI;
         [SerializeField] private GameObject hotbarUI;
 
+        public void OnPause(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                OpenPauseMenu();
+            }
+        }
+
+        public void OnResume(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                ClosePauseMenu();
+            }
+        }
+        
         public void OpenPauseMenu()
         {
             hotbarUI.SetActive(false);
             pauseMenuUI.SetActive(true);
+            playerInput.SwitchCurrentActionMap("PauseMenu");
             // Stop time
-            // Swap to PauseMenu action map
         }
 
         public void ClosePauseMenu()
         {
             hotbarUI.SetActive(true);
             pauseMenuUI.SetActive(false);
+            playerInput.SwitchCurrentActionMap("Player");
             // Resume time
-            // Swap to Player action map
         }
     }
 }
